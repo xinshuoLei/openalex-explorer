5eb7d1122970555f4f9d3b98dd3cc3d3da6aac07
xinshuoLei <leixshuo@gmail.com>
2022-06-10 20:05:28 -0500

    add navigation to concept and work page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,10 +11,19 @@ import Image from 'mui-image'
 import logo from '../images/OpenAlex-logo.png'
 import lightbulb from '../images/lightbulb.png'
 import paper from '../images/paper.png'
+import { useNavigate } from 'react-router-dom';
 
 const drawerWidth = 240;
 
 export const Sidebar = () => {
+    const navigate = useNavigate(); 
+    const toWorkPage = () => { 
+        navigate("/work");
+    }
+    const toConceptPage = () => {
+        navigate("/concept")
+    }
+
     return (
         <Drawer
         sx={{
@@ -31,18 +40,18 @@ export const Sidebar = () => {
         <Typography variant="h5" component="div" sx={{mt:2, ml:4}} fontFamily="monospace">
             Exploring
         </Typography>
-        <Image src={logo} height="10%" fit="contain"/>
+        <Image src={logo} height="10%" fit="contain" duration={0}/>
         <Divider />
         <List>
             <ListItem key="Work" disablePadding>
-              <ListItemButton>
-                <Image src={paper} fit="contain" height="15%" width="15%" sx={{ mr:3, ml:3}}/>
+              <ListItemButton onClick={toWorkPage}>
+                <Image src={paper} fit="contain" height="15%" width="15%" sx={{ mr:3, ml:3}} duration={0}/>
                 <Typography variant="h7" component="div" sx={{ mr:3, ml:4}} fontFamily="monospace">Work</Typography>
               </ListItemButton>
             </ListItem>
-            <ListItem key="Work" disablePadding>
-              <ListItemButton>
-                <Image src={lightbulb} fit="contain" height="15%" width="15%" sx={{ mr:3, ml:3}}/>
+            <ListItem key="Concept" disablePadding>
+              <ListItemButton onClick={toConceptPage}>
+                <Image src={lightbulb} fit="contain" height="15%" width="15%" sx={{ mr:3, ml:3}} duration={0}/>
                 <Typography variant="h7" component="div" sx={{ mr:3, ml:4}} fontFamily="monospace">Concept</Typography>
               </ListItemButton>
             </ListItem>
